e8df75e0f88aeef290cb8a4cd3ac5d3538b0bae5
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2023-08-22 15:10:36 +0900

    beam_search.cpp: multiple starts

--- beam_search.cpp
+++ beam_search.cpp
@@ -5,10 +5,6 @@ using namespace std;
 #define ll long long int
 #define ull unsigned long long int
 
-struct Input {
-    // TODO
-};
-
 using Action = int; // TODO
 using Evaluation = ll; // TODO
 using Hash = ull; // TODO
@@ -18,7 +14,7 @@ struct State {
 
     // TODO
 
-    State(const Input& input) {
+    State() {
         // TODO
     }
 
@@ -80,59 +76,95 @@ struct Node {
     }
 };
 
-void remove_node(vector<Node>& nodes, stack<int>& garbage, int v) {
-    while (true) {
-        assert(nodes[v].child == -1);
-
-        garbage.push(v);
-        if (nodes[v].left != -1) {
-            nodes[nodes[v].left].right = nodes[v].right;
-            if (nodes[v].right != -1) {
-                nodes[nodes[v].right].left = nodes[v].left;
-            }
-            break;
-        } else {
-            nodes[nodes[v].parent].child = nodes[v].right;
-            if (nodes[v].right != -1) {
-                nodes[nodes[v].right].left = -1;
-                break;
-            }
-            v = nodes[v].parent;
-        }
-    }
-}
-
 constexpr size_t beam_width = 100; // TODO
 constexpr int max_turn = 1000; // TODO
 constexpr size_t branches = 4; // TODO
 constexpr size_t nodes_size = 10000; // TODO
 
-vector<Action> beam_search(const Input& input) {
+pair<int,vector<Action>> beam_search(const vector<pair<State,Node>>& roots) {
 
     struct Candidate {
         Action action;
         int parent;
         Evaluation evaluation;
         Hash hash;
+        int tree_id;
 
-        Candidate(Action action, int parent, Evaluation evaluation, Hash hash) :
+        Candidate(Action action, int parent, Evaluation evaluation, Hash hash, int tree_id) :
             action(action),
             parent(parent),
             evaluation(evaluation),
-            hash(hash) {}
+            hash(hash),
+            tree_id(tree_id) {}
     };
 
-    State state(input);
+    struct Tree {
+        State state;
+        int id;
+        int root;
+        bool valid = true;
+
+        Tree(const State& state, int id) :
+            state(state),
+            id(id),
+            root(id) {}
+
+        pair<int,vector<Action>> dfs(vector<Node>& nodes, int turn, vector<Candidate>& candidates) {
+            if (!valid) {
+                return {-1, {}};
+            }
+            // go down the direct road
+            while (nodes[root].child != -1 && nodes[nodes[root].child].right == -1) {
+                root = nodes[root].child;
+                state.move_forward(nodes[root].action);
+            }
+            int v = root;
+            while (true) {
+                // go to child
+                while (nodes[v].child != -1) {
+                    v = nodes[v].child;
+                    state.move_forward(nodes[v].action);
+                }
+                assert(state.depth == turn);
+
+                if (state.finished()) {
+                    // found the solution
+                    vector<Action> path;
+                    path.reserve(turn);
+                    nodes[v].get_path(path, nodes);
+                    return {id, path};
+                }
+                // enumerate children
+                for (auto [action, evaluation, hash] : state.get_actions(nodes[v].evaluation, nodes[v].hash)) {
+                    candidates.emplace_back(Candidate(action, v, evaluation, hash, id));
+                }
+                while (v != root && nodes[v].right == -1) {
+                    // go to parent
+                    state.move_backward(nodes[v].action);
+                    v = nodes[v].parent;
+                }
+                if (v == root) {
+                    break;
+                }
+                // go to right sibling
+                state.move_backward(nodes[v].action);
+                v = nodes[v].right;
+                state.move_forward(nodes[v].action);
+            }
+            return {-1, {}};
+        }
+    };
+
+    vector<Tree> forest;
+    forest.reserve(roots.size());
 
     vector<Node> nodes;
     nodes.reserve(nodes_size);
-    nodes.emplace_back(Node((Action)0, -1, -1, 0, 0));
 
     stack<int> garbage; // store deleted node indices
 
     shared_ptr<vector<int>> temp_nodes(new vector<int>);
     temp_nodes->reserve(beam_width);
-    temp_nodes->push_back(0);
 
     shared_ptr<vector<int>> next_nodes(new vector<int>);
     next_nodes->reserve(beam_width);
@@ -142,58 +174,33 @@ vector<Action> beam_search(const Input& input) {
 
     vector<int> left_memo;
     left_memo.reserve(nodes_size);
-    left_memo.push_back(-1);
 
     vector<int> left_memo_using;
     left_memo_using.reserve(beam_width);
 
-    unordered_set<Hash> hashes(4 * beam_width);
+    unordered_set<Hash> hashes;
 
-    int root = 0;
-
+    for (size_t id = 0; id < roots.size(); ++id) {
+        forest.emplace_back(Tree(roots[id].first, id));
+        nodes.emplace_back(roots[id].second);
+        temp_nodes->push_back(id);
+        left_memo.push_back(-1);
+    }
     for (int turn = 0; turn < max_turn; ++turn) {
-        // go down the direct road
-        while (nodes[root].child != -1 && nodes[nodes[root].child].right == -1) {
-            root = nodes[root].child;
-            state.move_forward(nodes[root].action);
+        // depth first search on each tree
+        pair<int,vector<Action>> ret = {-1, {}};
+        for (Tree& tree : forest) {
+            pair<int,vector<Action>> path = tree.dfs(nodes, turn, candidates);
+            if (path.first != -1 && (ret.first == -1 || path.second.size() < ret.second.size())) {
+                ret = path;
+            }
         }
-        // Depth First Search
-        int v = root;
-        while (true) {
-            // go to child
-            while (nodes[v].child != -1) {
-                v = nodes[v].child;
-                state.move_forward(nodes[v].action);
-            }
-            assert(state.depth == turn);
-
-            if (state.finished()) {
-                // found the solution
-                vector<Action> path;
-                path.reserve(turn);
-                nodes[v].get_path(path, nodes);
-                // cerr << nodes.size() << endl;
-                return path;
-            }
-            // enumerate children
-            for (auto [action, evaluation, hash] : state.get_actions(nodes[v].evaluation, nodes[v].hash)) {
-                candidates.emplace_back(Candidate(action, v, evaluation, hash));
-            }
-            while (v != root && nodes[v].right == -1) {
-                // go to parent
-                state.move_backward(nodes[v].action);
-                v = nodes[v].parent;
-            }
-            if (v == root) {
-                break;
-            }
-            // go to right sibling
-            state.move_backward(nodes[v].action);
-            v = nodes[v].right;
-            state.move_forward(nodes[v].action);
+        if (ret.first != -1) {
+            // cerr << nodes.size() << endl;
+            return ret;
         }
         // sort candidates
-        sort(candidates.begin(), candidates.end(),
+        std::sort(candidates.begin(), candidates.end(),
             [](const Candidate& a, const Candidate& b) {
                 return a.evaluation > b.evaluation;
             }
@@ -205,7 +212,7 @@ vector<Action> beam_search(const Input& input) {
             path.reserve(turn);
             nodes[v].get_path(path, nodes);
             // cerr << nodes.size() << endl;
-            return path;
+            return {candidates[0].tree_id, path};
         }
         // make new nodes
         for (const Candidate& candidate : candidates) {
@@ -245,8 +252,33 @@ vector<Action> beam_search(const Input& input) {
 
         // delete redundant nodes
         for (int v : *temp_nodes) {
-            if (nodes[v].child == -1) {
-                remove_node(nodes, garbage, v);
+            if (nodes[v].child != -1) {
+                continue;
+            }
+            // delete v
+            while (true) {
+                assert(nodes[v].child == -1);
+                if (nodes[v].parent == -1) {
+                    // delete root
+                    assert(v < (int)forest.size());
+                    forest[v].valid = false;
+                    break;
+                }
+                garbage.push(v);
+                if (nodes[v].left != -1) {
+                    nodes[nodes[v].left].right = nodes[v].right;
+                    if (nodes[v].right != -1) {
+                        nodes[nodes[v].right].left = nodes[v].left;
+                    }
+                    break;
+                } else {
+                    nodes[nodes[v].parent].child = nodes[v].right;
+                    if (nodes[v].right != -1) {
+                        nodes[nodes[v].right].left = -1;
+                        break;
+                    }
+                    v = nodes[v].parent;
+                }
             }
         }
         // prepare for next iteration
