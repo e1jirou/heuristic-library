0bfd11f1aba6d3c9d8773519700a0fc14fde237f
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2023-09-20 15:22:47 +0900

    template.cpp: Timer (public or private)

--- template.cpp
+++ template.cpp
@@ -12,38 +12,41 @@ using namespace std;
 
 uniform_real_distribution<> uniform(0.0, 1.0);
 
-struct Timer {
-    chrono::system_clock::time_point start_time_;
-
-    void begin() {
-        start_time_ = chrono::system_clock::now();
-    }
-
-    double get_time() {
-        chrono::system_clock::time_point end_time = chrono::system_clock::now();
-        double elapsed_time = chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time_).count();
-        elapsed_time *= 1e-9; // nanoseconds -> seconds
-        return elapsed_time;
-    }
-
-    bool yet(double time_limit) {
-        return get_time() < time_limit;
-    }
-
-    double progress(double time_limit) {
-        return get_time() / time_limit;
-    }
-
-    bool annealing_scheduler(double profit, mt19937& engine, double time_limit, double t0, double t1) {
-        assert(0.0 <= t1 && t1 <= t0);
-        if (profit >= 0.0) {
-            return true;
-        } else {
-            double ratio = progress(time_limit);
-            double t = pow(t0, 1.0 - ratio) * pow(t1, ratio);
-            return uniform(engine) < pow(2.0, profit/t);
+class Timer {
+    public:
+        void begin() {
+            start_time_ = chrono::system_clock::now();
         }
-    }
+
+        double get_time() {
+            chrono::system_clock::time_point end_time = chrono::system_clock::now();
+            double elapsed_time = chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time_).count();
+            elapsed_time *= 1e-9; // nanoseconds -> seconds
+            return elapsed_time;
+        }
+
+        bool yet(double time_limit) {
+            return get_time() < time_limit;
+        }
+
+        double progress(double time_limit) {
+            return get_time() / time_limit;
+        }
+
+        bool annealing_scheduler(double profit, mt19937& engine, double time_limit, double t0, double t1) {
+            assert(0.0 <= t1 && t1 <= t0);
+            if (profit >= 0.0) {
+                return true;
+            } else {
+                double ratio = progress(time_limit);
+                double t = pow(t0, 1.0 - ratio) * pow(t1, ratio);
+                return uniform(engine) < pow(2.0, profit/t);
+            }
+        }
+
+    private:
+        chrono::system_clock::time_point start_time_;
+
 };
 
 constexpr double time_limit = 1.95; // TODO
