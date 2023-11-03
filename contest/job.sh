cd2afb825376154dba85edb8765543f167e2a7c6
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2023-11-03 22:03:52 +0900

    job.sh: AtCoder command

--- contest/job.sh
+++ contest/job.sh
@@ -2,6 +2,6 @@
 # command: nohup sh job.sh &
 
 export OMP_NUM_THREADS=20
-g++ -std=gnu++20 -O3 -Wall -Wextra -fopenmp main.cpp -o main
-./main < in.txt > out.txt
+g++ -std=gnu++20 -O2 -Wall -Wextra -fopenmp -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -I /usr/local/opt/boost/include -o a.out main.cpp
+./a.out < in.txt > out.txt
 rm -f main
