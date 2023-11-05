92c245655fa7eff537e32dd9740692f4e6226d32
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2023-11-05 12:41:39 +0900

    atcoder library

--- contest/job.sh
+++ contest/job.sh
@@ -2,6 +2,6 @@
 # command: nohup sh job.sh &
 
 export OMP_NUM_THREADS=20
-g++ -std=gnu++20 -O2 -Wall -Wextra -fopenmp -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -I /usr/local/opt/boost/include -o a.out main.cpp
+g++ -std=gnu++20 -O2 -Wall -Wextra -fopenmp -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -I /usr/local/opt/boost/include -I . -o a.out Main.cpp
 ./a.out < in.txt > out.txt
-rm -f main
+rm -f ./a.out
