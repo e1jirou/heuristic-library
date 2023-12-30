b5c7545f57d4af188963316f63a40f84091bfe8d
pekingenjin <kitahara-hiroaki@g.ecc.u-tokyo.ac.jp>
2023-12-30 17:58:08 +0900

    articulation_point.cpp: low_link

--- articulation_point.cpp
+++ articulation_point.cpp
@@ -52,3 +52,31 @@ using namespace std;
 // const bitset<512> is_connected = make_connection_judge();
 
 const bitset<512> is_connected_pattern("11111111110111011111101111010001110011111100110110001011000000011111111111011101100000001101000111001111110011010000000000000001111111110000000010000000000000000000000000000000000000000000000011111111110111011000000011010001000000000000000000000000000000011111111111011101100000000000000011001111110011011000101100000001111111111101110100000000000000001100111111001101000000000000000111111111000000001000000000000000110011110000000010001011000000011111111111011101100000001101000111001111110011011000101111011110");
+
+// strict solver
+int low_link_for_undirected_grpah(int u, int i, int parent, vector<int>& ord, vector<int>& low, vector<bool>& is_articulation_point, const vector<vector<int>>& edges) {
+    assert(ord[u] == -1);
+    ord[u] = i++;
+    int num_children = 0;
+    for (int v : edges[u]) {
+        if (ord[v] == -1) {
+            // forward edge
+            ++num_children;
+            i = low_link_for_undirected_grpah(v, i, u, ord, low, is_articulation_point, edges);
+            low[u] = min(low[u], low[v]);
+            if (ord[u] && ord[u] <= low[v]) {
+                is_articulation_point[u] = true;
+            }
+        } else {
+            // backward edge
+            if (v != parent && ord[v] < ord[u] - 1) {
+                low[u] = min(low[u], ord[v]);
+            }
+        }
+    }
+    if (ord[u] == 0 && num_children >= 2) {
+        // root
+        is_articulation_point[u] = true;
+    }
+    return i;
+}
